386c898c1d74c964b47495909469d8db18bdb09a
John <johhaneshongan@gmail.com>
2020-04-21 00:59:55 -0700

    datasource updated

--- server/datasources/user.js
+++ server/datasources/user.js
@@ -16,8 +16,13 @@ class UserAPI extends DataSource {
 // User can be called with an argument that includes email, but it doesn't 
 // have to be. If the user is already on the context, it will use that user instead 
 
+    async getUsers() {
+        const users = await this.store.users
+        return users
+    }
+
 
     
 }
 
-export default UserAPI
+module.exports = UserAPI; 
--- server/index.js
+++ server/index.js
@@ -3,11 +3,20 @@ const typeDefs = require('./schema');
 const QuakeAPI = require('./datasources/quake')
 const resolvers = require('./resolvers')
 
+// create store method 
+const {createStore} = require('./utils')
+
+const userAPI = require('./datasources/user')
+// create store object
+
+const store = createStore()
+
 const server = new ApolloServer ({
     typeDefs,
     resolvers,
     dataSources: ()=>({
-        quakeAPI: new QuakeAPI()
+        quakeAPI: new QuakeAPI(),
+        userAPI: new UserAPI({store})
     })})
 
 server.listen().then(({url})=>{
