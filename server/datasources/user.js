6ece78e4a0d406f1105703c562605f63a59fa707
John <johhaneshongan@gmail.com>
2020-04-20 23:32:31 -0700

    update db

--- server/datasources/user.js
+++ server/datasources/user.js
@@ -1,1 +1,23 @@
-const {DataSource} = require('apollo-datasource')
+const {DataSource} = require('apollo-datasource');
+
+class UserAPI extends DataSource {
+    constructor({store}){
+        super();
+        this.store = store;
+    }
+    // this is a function that gets called by Apollo Server when being setup
+    // This function gets called with the data source config including things
+    // like cache and context . We'll assign this.context to the request context
+    // here, so we can know about the user making requests
+
+    initialize(config){
+        this.context = config.context;
+    }
+// User can be called with an argument that includes email, but it doesn't 
+// have to be. If the user is already on the context, it will use that user instead 
+
+
+    
+}
+
+export default UserAPI
